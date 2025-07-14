f60653c2981ba81821ef4c32835ca47f76b4157f
David Válek <david.valek17@gmail.com>
2025-07-14 20:47:47 +0200

    Create backtest endpoint

--- frontend/backend/src/main/java/com/github/sol239/javafi/backend/controllers/BacktestController.java
+++ frontend/backend/src/main/java/com/github/sol239/javafi/backend/controllers/BacktestController.java
@@ -1,4 +1,61 @@
 package com.github.sol239.javafi.backend.controllers;
 
+import com.github.sol239.javafi.backend.services.BacktestService;
+import com.github.sol239.javafi.backend.services.TableService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/backtest")
 public class BacktestController {
+
+    private final BacktestService backtestService;
+
+    public BacktestController(BacktestService backtestService) {
+        this.backtestService = backtestService;
+    }
+
+
+    @PostMapping("run")
+    public void runBacktest(
+            @RequestParam String tableName,
+            @RequestParam String balance,
+            @RequestParam String leverage,
+            @RequestParam String fee,
+            @RequestParam String takeProfit,
+            @RequestParam String stopLoss,
+            @RequestParam String amount,
+            @RequestParam String maxTrades,
+            @RequestParam String delaySeconds,
+            @RequestParam String dateRestriction,
+            @RequestParam String tradeLifeSpanSeconds,
+            @RequestParam String openClause,
+            @RequestParam String closeClause,
+            @RequestParam String stopLossEnabled,
+            @RequestParam String takeProfitEnabled
+    ) {
+        System.out.println("Running backtest.");
+        backtestService.runBackTest(
+                tableName, // This should be replaced with the actual table name if needed
+                balance,
+                leverage,
+                fee,
+                takeProfit,
+                stopLoss,
+                amount,
+                maxTrades,
+                delaySeconds,
+                dateRestriction,
+                tradeLifeSpanSeconds,
+                openClause,
+                closeClause,
+                stopLossEnabled,
+                takeProfitEnabled
+        );;
+        System.out.println("Backtest run completed.");
+
+    }
+
 }
---
+++ frontend/backend/src/main/java/com/github/sol239/javafi/backend/services/BacktestService.java
@@ -0,0 +1,45 @@
+package com.github.sol239.javafi.backend.services;
+
+import com.github.sol239.javafi.backend.utils.backtesting.BacktestingExecutor;
+import com.github.sol239.javafi.backend.utils.backtesting.Setup;
+import com.github.sol239.javafi.backend.utils.backtesting.Strategy;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BacktestService {
+    private final BacktestingExecutor backtestingExecutor;
+
+    public BacktestService(BacktestingExecutor backtestingExecutor) {
+        this.backtestingExecutor = backtestingExecutor;
+    }
+
+    public void runBackTest(
+            String tableName,
+            String balance,
+            String leverage,
+            String fee,
+            String takeProfit,
+            String stopLoss,
+            String amount,
+            String maxTrades,
+            String delaySeconds,
+            String dateRestriction,
+            String tradeLifeSpanSeconds,
+            String openClause,
+            String closeClause,
+            String stopLossEnabled,
+            String takeProfitEnabled) {
+
+        double riskReward = 1;
+        Setup setup = new Setup(Double.parseDouble(balance), Double.parseDouble(leverage), Double.parseDouble(fee), Double.parseDouble(takeProfit), Double.parseDouble(stopLoss), Double.parseDouble(amount), riskReward, Integer.parseInt(maxTrades), Integer.parseInt(delaySeconds), dateRestriction, Integer.parseInt(tradeLifeSpanSeconds));
+        Strategy strategy = new Strategy(openClause, closeClause, setup);
+
+        backtestingExecutor.addStrategy(strategy);
+        backtestingExecutor.createStrategiesColumns(tableName);
+
+        backtestingExecutor.updateStrategiesColumns(tableName);   // TODO: does not have to be executed each time
+        backtestingExecutor.run(tableName, setup.tradeLifeSpanSeconds, strategy.takeProfit, strategy.stopLoss, "C:/Users/David/Desktop/result.json", setup.dateRestriction);
+
+    }
+
+}
